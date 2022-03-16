13687b90d35a41d306455754bf8f901fdf3fb069
turbofish <ryokotmng@gmail.com>
2022-03-16 22:24:41 +0900

    ch-3-ex-1 move client source code to complete folder

--- cmd/ch-3-ex-1/client/completed/main.go
+++ cmd/ch-3-ex-1/client/completed/main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"bytes"
 	"embed"
-	"encoding/base64"
+	"encoding/json"
+	"fmt"
 	"html/template"
+	"io"
 	"net/http"
 	"net/url"
 
 	"github.com/gin-gonic/gin"
+
+	"github.com/ryokotmng/oauth-in-action-code-go/pkg"
 )
 
 // authorization server information
@@ -30,6 +35,10 @@ var demoClient = client{
 	clientId:     "oauth-client-1",
 	clientSecret: "oauth-client-secret-1",
 	redirectURIs: []string{"http://localhost:9000/callback"},
+}
+
+type tokenResponseBody struct {
+	AccessToken string `json:"access_token"`
 }
 
 var (
@@ -58,43 +67,130 @@ func main() {
 }
 
 func authorize(c *gin.Context) {
+	state = pkg.RandomString(32)
+	authorizeUrl := buildUrl(authorizationEndpoint, &map[string]string{
+		"response_type": "code",
+		"client_id":     demoClient.clientId,
+		"redirect_uri":  demoClient.redirectURIs[0],
+		"state":         state,
+	}, nil)
 
-	/*
-	 * Send the user to the authorization server
-	 */
-
+	c.Writer.Status()
+	c.Redirect(302, authorizeUrl)
 }
 
 func callback(c *gin.Context) {
 
-	/*
-	 * Parse the response from the authorization server and get a token
-	 */
+	// it's an error response, act accordingly
+	if c.Errors != nil {
+		c.HTML(http.StatusBadRequest, "error.html", gin.H{"error": c.Errors})
+		return
+	}
 
+	if s := c.Query("state"); s != state {
+		fmt.Printf("State DOES NOT MATCH: expected %s got %s \n", state, s)
+		c.HTML(http.StatusBadRequest, "error.html", gin.H{"error": "State value did not match"})
+		return
+	}
+
+	code := c.Query("code")
+
+	formData, err := json.Marshal(map[string]string{
+		"grant_type":   "authorization_code",
+		"code":         code,
+		"redirect_uri": demoClient.redirectURIs[0],
+	})
+	if err != nil {
+		c.HTML(http.StatusBadRequest, "error.html", gin.H{"error": err.Error()})
+		return
+	}
+
+	req, err := http.NewRequest(http.MethodPost, tokenEndpoint, bytes.NewBuffer(formData))
+	if err != nil {
+		c.HTML(http.StatusBadRequest, "error.html", gin.H{"error": err.Error()})
+		return
+	}
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Set("Authorization", "Basic "+encodeClientCredentials(demoClient.clientId, demoClient.clientSecret))
+	tokRes, err := http.DefaultClient.Do(req)
+	if err != nil {
+		c.HTML(http.StatusBadRequest, "error.html", gin.H{"error": err.Error()})
+	}
+
+	fmt.Printf("Requesting access token for code %s \n", code)
+	if tokRes.StatusCode >= 200 && tokRes.StatusCode < 300 {
+		body, err := io.ReadAll(tokRes.Body)
+		if err != nil {
+			c.HTML(http.StatusBadRequest, "error.html", gin.H{"error": err.Error()})
+		}
+		defer func() {
+			err := tokRes.Body.Close()
+			if err != nil {
+				c.HTML(http.StatusBadRequest, "error.html", gin.H{"error": err.Error()})
+			}
+		}()
+		var resBody tokenResponseBody
+		err = json.Unmarshal(body, &resBody)
+		if err != nil {
+			c.HTML(http.StatusBadRequest, "error.html", gin.H{"error": fmt.Sprintf("Unable to fetch access token, err: %v", err.Error())})
+			return
+		}
+		accessToken = resBody.AccessToken
+		fmt.Printf("Got access token: %s \n", accessToken)
+
+		c.HTML(http.StatusOK, "index.html", gin.H{"accessToken": accessToken, "scope": scope})
+		return
+	}
+
+	c.HTML(http.StatusBadRequest, "error.html", gin.H{"error": fmt.Sprintf("Unable to fetch access token, server response: %v", tokRes.StatusCode)})
 }
 
 func fetchResource(c *gin.Context) {
+	if accessToken == "" {
+		c.HTML(http.StatusBadRequest, "error.html", gin.H{"error": "Missing Access Token"})
+		return
+	}
 
-	/*
-	 * Use the access token to call the resource server
-	 */
+	fmt.Printf("Making request with access token %s \n", accessToken)
 
+	req, err := http.NewRequest("POST", protectedResource, nil)
+	if err != nil {
+		return
+	}
+	req.Header.Set("Authorization", "Bearer "+accessToken)
+	resource, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return
+	}
+	if resource.StatusCode >= 200 && resource.StatusCode < 300 {
+		bodyReader := resource.Body
+		body, err := io.ReadAll(bodyReader)
+		if err != nil {
+			c.HTML(http.StatusBadRequest, "error.html", gin.H{"error": err.Error()})
+		}
+		var bodyData interface{}
+		json.Unmarshal(body, &bodyData)
+		c.HTML(http.StatusOK, "data.html", gin.H{"resource": bodyData})
+		return
+	}
+	accessToken = ""
+	c.HTML(resource.StatusCode, "error.html", gin.H{"error": resource.StatusCode})
 }
 
-func buildUrl(base string, options, hash map[string]string) *url.URL {
+func buildUrl(base string, options, hash *map[string]string) string {
 	newUrl, err := url.Parse(base)
 	if err != nil {
-		return nil
+		return ""
 	}
 
 	q := newUrl.Query()
-	for k, v := range options {
+	for k, v := range *options {
 		q.Set(k, v)
 	}
 	newUrl.RawQuery = q.Encode()
-	return newUrl
+	return newUrl.String()
 }
 
-func encodeClientCredentials(clientId, clientSecret string) *base64.Encoding {
-	return base64.NewEncoding(url.QueryEscape(clientId) + ":" + url.QueryEscape(clientSecret))
+func encodeClientCredentials(clientId, clientSecret string) string {
+	return url.QueryEscape(clientId) + ":" + url.QueryEscape(clientSecret)
 }
--- cmd/ch-3-ex-1/client/main.go
+++ cmd/ch-3-ex-1/client/main.go
@@ -1,18 +1,12 @@
 package main
 
 import (
-	"bytes"
 	"embed"
-	"encoding/json"
-	"fmt"
 	"html/template"
-	"io"
 	"net/http"
 	"net/url"
 
 	"github.com/gin-gonic/gin"
-
-	"github.com/ryokotmng/oauth-in-action-code-go/pkg"
 )
 
 // authorization server information
@@ -66,114 +60,27 @@ func main() {
 }
 
 func authorize(c *gin.Context) {
-	state = pkg.RandomString(32)
-	authorizeUrl := buildUrl(authorizationEndpoint, &map[string]string{
-		"response_type": "code",
-		"client_id":     demoClient.clientId,
-		"redirect_uri":  demoClient.redirectURIs[0],
-		"state":         state,
-	}, nil)
 
-	c.Writer.Status()
-	c.Redirect(302, authorizeUrl)
+	/*
+	 * Send the user to the authorization server
+	 */
+
 }
 
 func callback(c *gin.Context) {
 
-	// it's an error response, act accordingly
-	if c.Errors != nil {
-		c.HTML(http.StatusBadRequest, "error.html", gin.H{"error": c.Errors})
-		return
-	}
+	/*
+	 * Parse the response from the authorization server and get a token
+	 */
 
-	if s := c.Query("state"); s != state {
-		fmt.Printf("State DOES NOT MATCH: expected %s got %s \n", state, s)
-		c.HTML(http.StatusBadRequest, "error.html", gin.H{"error": "State value did not match"})
-		return
-	}
-
-	code := c.Query("code")
-
-	formData, err := json.Marshal(map[string]string{
-		"grant_type":   "authorization_code",
-		"code":         code,
-		"redirect_uri": demoClient.redirectURIs[0],
-	})
-	if err != nil {
-		c.HTML(http.StatusBadRequest, "error.html", gin.H{"error": err.Error()})
-		return
-	}
-
-	req, err := http.NewRequest(http.MethodPost, tokenEndpoint, bytes.NewBuffer(formData))
-	if err != nil {
-		c.HTML(http.StatusBadRequest, "error.html", gin.H{"error": err.Error()})
-		return
-	}
-	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-	req.Header.Set("Authorization", "Basic "+encodeClientCredentials(demoClient.clientId, demoClient.clientSecret))
-	tokRes, err := http.DefaultClient.Do(req)
-	if err != nil {
-		c.HTML(http.StatusBadRequest, "error.html", gin.H{"error": err.Error()})
-	}
-
-	fmt.Printf("Requesting access token for code %s \n", code)
-	if tokRes.StatusCode >= 200 && tokRes.StatusCode < 300 {
-		body, err := io.ReadAll(tokRes.Body)
-		if err != nil {
-			c.HTML(http.StatusBadRequest, "error.html", gin.H{"error": err.Error()})
-		}
-		defer func() {
-			err := tokRes.Body.Close()
-			if err != nil {
-				c.HTML(http.StatusBadRequest, "error.html", gin.H{"error": err.Error()})
-			}
-		}()
-		var resBody tokenResponseBody
-		err = json.Unmarshal(body, &resBody)
-		if err != nil {
-			c.HTML(http.StatusBadRequest, "error.html", gin.H{"error": fmt.Sprintf("Unable to fetch access token, err: %v", err.Error())})
-			return
-		}
-		accessToken = resBody.AccessToken
-		fmt.Printf("Got access token: %s \n", accessToken)
-
-		c.HTML(http.StatusOK, "index.html", gin.H{"accessToken": accessToken, "scope": scope})
-		return
-	}
-
-	c.HTML(http.StatusBadRequest, "error.html", gin.H{"error": fmt.Sprintf("Unable to fetch access token, server response: %v", tokRes.StatusCode)})
 }
 
 func fetchResource(c *gin.Context) {
-	if accessToken == "" {
-		c.HTML(http.StatusBadRequest, "error.html", gin.H{"error": "Missing Access Token"})
-		return
-	}
 
-	fmt.Printf("Making request with access token %s \n", accessToken)
+	/*
+	 * Use the access token to call the resource server
+	 */
 
-	req, err := http.NewRequest("POST", protectedResource, nil)
-	if err != nil {
-		return
-	}
-	req.Header.Set("Authorization", "Bearer "+accessToken)
-	resource, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return
-	}
-	if resource.StatusCode >= 200 && resource.StatusCode < 300 {
-		bodyReader := resource.Body
-		body, err := io.ReadAll(bodyReader)
-		if err != nil {
-			c.HTML(http.StatusBadRequest, "error.html", gin.H{"error": err.Error()})
-		}
-		var bodyData interface{}
-		json.Unmarshal(body, &bodyData)
-		c.HTML(http.StatusOK, "data.html", gin.H{"resource": bodyData})
-		return
-	}
-	accessToken = ""
-	c.HTML(resource.StatusCode, "error.html", gin.H{"error": resource.StatusCode})
 }
 
 func buildUrl(base string, options, hash *map[string]string) string {
