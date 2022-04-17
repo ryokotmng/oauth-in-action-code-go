7f74746faee529abbbd7ddc8abec6e2f63008b64
turbofish <ryokotmng@gmail.com>
2022-04-17 22:07:37 +0900

    add godoc coments to exported variables and functions

--- pkg/contains.go
+++ pkg/contains.go
@@ -1,5 +1,6 @@
 package pkg
 
+// Contains returns whether the slice contains the specified element or not
 func Contains(sl []string, str string) bool {
 	for _, s := range sl {
 		if s == str {
--- pkg/redis.go
+++ pkg/redis.go
@@ -2,10 +2,12 @@ package pkg
 
 import "github.com/go-redis/redis/v8"
 
+// TokenRecord keeps client id
 type TokenRecord struct {
 	ClientID string `json:"client_id"`
 }
 
+// NewRedisClient returns redis client with default options
 func NewRedisClient() *redis.Client {
 	return redis.NewClient(&redis.Options{
 		Addr:     "localhost:6379",
